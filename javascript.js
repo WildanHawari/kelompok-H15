eab1e13365b8b41e4701b6cc75022c330d2bca05
WildanHawari <64912105+WildanHawari@users.noreply.github.com>
2020-10-15 22:44:07 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@
     let getFoto = document.getElementById("myface");
 
     if (elem == "wildan") { 
-        getFoto.src = "img/profile.png";
+        getFoto.src = "/img/profile.png";
         getDeskripsi.innerHTML = " <p>Halo nama saya <b>Muhammad Wildan Hawari</b>. <br> Saya berdomisili di Kota Jakarta Timur. Hobby saya Pencak Silat.<br>Umur saya 16 Tahun</p > ";
     } else {
-        getFoto.src = "img/mic.png";
+        getFoto.src = "/img/mic.png";
         getDeskripsi.innerHTML = " <p><p>Halo nama saya <b>Michael Danu Ekklasiya</b>. <br> Saya berdomisili di Kota Bogor. Hobby saya Bermain Musik.<br>Umur saya 16 Tahun</p > </p > "; //deskripsi lu
     }
 
